7f045764e857ab42aaa8e4adf17f3bef5cfd4c7d
Karan Patil <kpkaranpatil600@gmail.com>
2022-05-28 10:33:31 -0400

    starting with vowels

---
+++ Basic Select/10. Weather Observation Station 6.sql
@@ -0,0 +1,15 @@
+/*  
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+The STATION table is described as follows:
+
+Field   | type
+
+ID      | NUMBER
+CITY    | VARCHAR2(21)
+STATE   | VARCHAR2(2)
+LAT_N   | NUMBER
+LONG_N  | NUMBER
+*/  
+    
+SELECT DISTINCT(CITY) FROM STATION
+WHERE CITY LIKE '[a,e,i,o,u]%';  
