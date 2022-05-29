59b21fc98bb909221b9b221b259edd950c3a25d0
Karan Patil <kpkaranpatil600@gmail.com>
2022-05-29 15:41:18 -0400

    start and end with vowels

---
+++ Basic Select/12. Weather Observation Station 8.sql
@@ -0,0 +1,15 @@
+/*  
+Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+The STATION table is described as follows:
+
+Field   | type
+
+ID      | NUMBER
+CITY    | VARCHAR2(21)
+STATE   | VARCHAR2(2)
+LAT_N   | NUMBER
+LONG_N  | NUMBER
+*/  
+    
+SELECT DISTINCT(CITY) FROM STATION
+WHERE CITY LIKE '[a,e,i,o,u]%[a,e,i,o,u]';  
