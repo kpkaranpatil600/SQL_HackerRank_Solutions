96038192f002df2a5252970f37031d26ea151a53
Karan Patil <kpkaranpatil600@gmail.com>
2022-05-30 08:26:27 -0400

    Do not end with vowels

---
+++ Basic Select/14. Weather Observation Station 10.sql
@@ -0,0 +1,17 @@
+/*  
+# Author: Karan Patil
+
+Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+The STATION table is described as follows:
+
+Field   | type
+
+ID      | NUMBER
+CITY    | VARCHAR2(21)
+STATE   | VARCHAR2(2)
+LAT_N   | NUMBER
+LONG_N  | NUMBER
+*/  
+    
+SELECT DISTINCT(CITY) FROM STATION
+WHERE CITY NOT LIKE '%[a,e,i,o,u]'; 
