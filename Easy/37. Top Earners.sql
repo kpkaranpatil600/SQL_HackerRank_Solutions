d21abcc6cb5c9da8d5c0a620ab80399bf93bcb8a
Karan Patil <kpkaranpatil600@gmail.com>
2024-03-25 15:21:57 -0400

    new query to solve

--- Easy/37. Top Earners.sql
+++ Easy/37. Top Earners.sql
@@ -7,3 +7,10 @@ SELECT sn, COUNT(sn)
 FROM TEMP
 WHERE rn = (SELECT MIN(rn) FROM TEMP)
 GROUP BY sn;
+
+----------------------------------------------------------------
+
+SELECT (salary * months) as sn, COUNT(1)
+FROM employee
+WHERE salary*months in (SELECT MAX(salary * months) FROM employee)
+GROUP BY (salary * months);
